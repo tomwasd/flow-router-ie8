6f536ae8656530fb18227087ad12947d796cfeab
tomwasd <tom@wasd.co.uk>
2015-08-10 13:38:27 +0100

    Bumped version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tomwasd:flow-router-ie8',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Polyfills required for IE8 support for flow-router',
   // URL to the Git repository containing the source code for this package.
